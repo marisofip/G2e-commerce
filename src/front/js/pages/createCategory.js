179d22884ac848335ceaa508d912fe93224a48aa
roacv <roacv@hotmail.com>
2023-01-27 22:46:27 +0000

    Create categoia f

--- src/front/js/pages/createCategory.js
+++ src/front/js/pages/createCategory.js
@@ -2,30 +2,28 @@ import React from 'react';
 
 export const NewCategory = () => {
     return (
-        <div className='container'>
+        <div className='container w-50 justify-content-center mt-5  mb-5'>
             <div className='row'>
-                <div className='col-12 p-5'>
-                    <h1 className='titleCategory' style={{ textAlign: 'center' }}>Crear Categoria</h1>
+                <div className='col-12'>
+                    <h1 className='fw-normal bg-secondary text-white py-3 mb-5 rounded-3 text-center'>Crear Categoría</h1>
                 </div>
-                <div className='col-12 p-5 ml-50' style={{ width: '500px', height: '500px', backgroundColor: 'gray' }}>
-                </div>
-                <div className='col-12 p-5'>
-                    <div class="mb-3">
-                        <label for="exampleFormControlInput1" className="form-label">Nombre de Categoria</label>
-                        <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="Escribe aqui" />
-                    </div>
-                    <div class="mb-3">
-                        <label for="exampleFormControlInput2" className="form-label">Tipo de Categoria</label>
+                <div className='col-12'>
+                    <div className="mb-3">
+                        <label htmlFor="exampleFormControlInput1" className="form-label">Nombre de Categoria</label>
                         <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="Escribe aqui" />
                     </div>
                     <div className="mb-3">
-                        <label for="exampleFormControlTextarea1" className="form-label">Descripcion</label>
+                        <label htmlFor="exampleFormControlInput2" className="form-label">Tipo de Categoria</label>
+                        <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="Escribe aqui" />
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="exampleFormControlTextarea1" className="form-label">Descripcion</label>
                         <textarea className="form-control" id="Descripcion" rows="3"></textarea>
                     </div>
                     <div className='paymentButtom2 col-12'>
 
-                        <button type="button" class="btn btn-success m-2">Guardar</button>
-                        <button type="button" class="btn btn-danger">Cancelar</button>
+                        <button type="button" className="btn btn-success m-2">Guardar</button>
+                        <button type="button" className="btn btn-danger">Cancelar</button>
                     </div>
 
                 </div>
--- src/front/js/pages/registro_usuario.js
+++ src/front/js/pages/registro_usuario.js
@@ -4,72 +4,69 @@ export const RegistroUsuario = () => {
   return (
     <>
       <div className="container w-50 justify-content-center mt-5  mb-5">
-        <h1 className="fw-normal text-white shadow-none p-3 mb-5 bg-info rounded-4 "
-          style={{ textAlign: "center", boxShadow: "0.2" }}>
+        <h1 className="fw-normal bg-secondary text-white py-3 mb-5 rounded-3 text-center">
             Registro de Usuario
         </h1>
         <form className="row g-3">
 
           <div className="form-floating col-md-6">
             <input type="text" className="form-control" id="name" placeholder="Nombre"/>
-            <label for="name">Nombre</label>
+            <label htmlFor="name">Nombre</label>
           </div>
           <div className="form-floating col-md-6">
             <input type="text" className="form-control" id="apellidopaterno" placeholder="Apellido paterno"/>
-            <label for="apellidopaterno">Apellido</label>
+            <label htmlFor="apellidopaterno">Apellido</label>
           </div>
           <div className="form-floating col-md-6">
             <input type="text" className="form-control" id="rut" placeholder="RUT"/>
-            <label for="rut">RUT</label>
+            <label htmlFor="rut">RUT</label>
           </div>
           <div className="form-floating col-md-12">
             <input type="text" className="form-control" id="direccion" placeholder="Direccion"/>
-            <label for="direccion">Calle número, Depto oficina</label>
+            <label htmlFor="direccion">Calle número, Depto oficina</label>
           </div>
           <div className="form-floating col-md-4">
-            <select class="form-select" id="comuna" aria-label="Seleccione comuna">
-              <option selected>Comuna</option>
+            <select className="form-select" id="comuna" aria-label="Seleccione comuna">
+              <option value="0">Comuna</option>
               <option value="1">Comuna 1</option>
               <option value="2">Comuna 2</option>
               <option value="3">Comuna 3</option>
             </select>
-            <label for="comuna">Seleccione comuna</label>
+            <label htmlFor="comuna">Seleccione comuna</label>
           </div>
           <div className="form-floating col-md-4">
-            <select class="form-select" id="ciudad" aria-label="Seleccione ciudad">
-              <option selected>Ciudad</option>
+            <select className="form-select" id="ciudad" aria-label="Seleccione ciudad">
               <option value="1">Ciudad 1</option>
               <option value="2">Ciudad 2</option>
               <option value="3">Ciudad 3</option>
             </select>
-            <label for="ciudad">Seleccione ciudad</label>
+            <label htmlFor="ciudad">Seleccione ciudad</label>
           </div>
           <div className="form-floating col-md-4">
-            <select class="form-select" id="region" aria-label="Seleccione región">
-              <option selected>Región</option>
+            <select className="form-select" id="region" aria-label="Seleccione región">
               <option value="1">Región 1</option>
               <option value="2">Región 2</option>
               <option value="3">Región 3</option>
             </select>
-            <label for="region">Seleccione región</label>
+            <label htmlFor="region">Seleccione región</label>
           </div>
           <div className="form-floating col-md-6">
             <input type="text" className="form-control" id="codigopostal" placeholder="Código postal"/>
-            <label for="codigopostal">Código postal</label>
+            <label htmlFor="codigopostal">Código postal</label>
           </div>
           <div className="form-floating col-md-6">
             <input type="text" className="form-control" id="fono" placeholder="Teléfono"/>
-            <label for="fono">Número de teléfono</label>
+            <label htmlFor="fono">Número de teléfono</label>
           </div>          
           <div className="form-floating col-md-6">
             <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com"/>
-            <label for="floatingInput">Correo "name@example.com"</label>
+            <label htmlFor="floatingInput">Correo "name@example.com"</label>
           </div>
           <div className="form-floating col-md-6">
             <input type="password" className="form-control" id="floatingPassword" placeholder="Password"/>
-            <label for="floatingPassword">Contraseña</label>
+            <label htmlFor="floatingPassword">Contraseña</label>
           </div>
-          <div className="d-grid gap-4 d-md-flex justify-content-md-end pt-5">
+          <div className="d-grid gap-4 d-md-flex justify-content-md-end pt-2">
             <button className="btn btn-success mt-2" type="button">
               Siguiente
             </button>
