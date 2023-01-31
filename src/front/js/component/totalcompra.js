5cd9c4ce2b10570df49bbc08d7a4cfebafa2a8ba
roacv <roacv@hotmail.com>
2023-01-31 05:12:18 +0000

    Shopping-Cart, carrito, totalcompra

--- src/front/js/component/totalcompra.js
+++ src/front/js/component/totalcompra.js
@@ -1,15 +1,35 @@
-import React from "react";
+import React , { useContext , useState, useEffect} from "react";
+import { Context } from "../store/appContext";
+import { NumericFormat } from 'react-number-format';
+
 
 export const TotalCompra = () => {
+    const { store, actions } = useContext(Context);
+    const [vSumTotal, setVSumTotal] = useState(0);
+
+    useEffect(() => {
+        const sumar = () => {
+          const sumar = store.carShopping.map((carro, index) => (parseFloat(carro.precio)*parseFloat(carro.cantidad)))
+            .reduce((previous, current) => {
+              return previous + current;
+            }, 0);
+          setVSumTotal(sumar);
+        };
+        sumar();
+      });
+
+
     return (
             <div className="">
                     <h5 className="card-title">Total de la Compra</h5>
-                    <div className="border-top border-bottom border-2 mx-auto my-3" style={{width:'300px'}}>
-                        <p className="text-end my-3 me-2">Sub-total   $000.000.-</p>
-                        <p className="text-end my-3 me-2">Despacho    $000.000.-</p>
+                    <div className="border-top border-bottom border-2 mx-auto my-3" style={{width:'400px'}}>
+                        <div className="text-end my-3 me-2">
+                            <NumericFormat value={vSumTotal} prefix="Sub-total $" thousandSeparator="." decimalSeparator="," displayType="text"   />
+                        </div>
+                        <p className="text-end my-3 me-2">Despacho    $0</p>
                     </div>
-                    <div className="mx-auto my-3" style={{width:'300px'}}>
-                        <h6 className="text-end my-3 me-2">Despacho    $000.000.-</h6>
+                    <div className="my-3" style={{width:'393px'}}>
+                        <NumericFormat value={vSumTotal} prefix="Total $" thousandSeparator="." decimalSeparator="," displayType="text" renderText={(value) => <h6 className="text-end">{value}</h6>}/>
                     </div>
             </div>
     );
--- src/front/js/pages/shopping-cart.js
+++ src/front/js/pages/shopping-cart.js
@@ -2,6 +2,7 @@ import React, { useContext } from "react";
 import { Context } from "../store/appContext";
 import "../../styles/home.css";
 import { TotalCompra } from "../component/totalcompra";
+import { Link } from "react-router-dom";
 
 export const ShoppingCart = () => {
 	const { store, actions } = useContext(Context);
@@ -24,6 +25,9 @@ export const ShoppingCart = () => {
 										<h5 className="card-title">{carro.nombre}</h5>
 										<p className="card-text text-end">Cantidad : {carro.cantidad}</p>
 										<p className="card-text text-end">Precio : ${carro.precio}</p>
+										<p className="card-text text-end">
+											<Link onClick={() => actions.eliminarCarShop(index)} className="far fa-trash-alt fa-lg link-danger" to=""></Link>
+										</p>
 									</div>
 								</div>
 							</div>
@@ -37,11 +41,11 @@ export const ShoppingCart = () => {
 				</div>
 			</div>
 			<div className="d-grid gap-3 d-md-flex justify-content-md-end">
-				<button className="btn btn-success mt-2" type="button">
-				Siguiente
-				</button>
+				<Link className="btn btn-success mt-2" type="button" to="/shipping">
+					Siguiente
+				</Link>
 				<button className="btn btn-secondary mt-2" type="button">
-				Cancelar
+					Cancelar
 				</button>
           	</div>
 		</div>
