46e0661f88af991c88c0b06e2a82d9410a377387
marisofip <marisofipalencia@gmail.com>
2023-02-02 02:11:41 +0000

    no va

--- src/front/js/component/carrito.js
+++ src/front/js/component/carrito.js
@@ -1,41 +1,73 @@
-import React, { useContext } from "react";
+import React, { useContext,useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { Context } from "../store/appContext";
 
 //import { FaTrashAlt } from 'react-icons/fa';
 
-const CarShopping = () => { 
-	const { store, actions } = useContext(Context);
-	const x = store.mostrarCarShop;
+const CarShopping = () => {
+  const { store, actions } = useContext(Context);
+  const x = store.mostrarCarShop;
+  const [vSumTotal, setVSumTotal] = useState(0);
 
-	return (
-	
-		<div className="carShopping">
-			{store.carShopping.map((el, index) => {
-					return (
-            <button id="carShopping" className="btn btn-outline-secondary mt-2 ms-2" onClick={() => actions.setMostrarCarShop()} >
-                <i className="fa-solid fa-cart-shopping fa-2x text-body"key={index}/>
-                {" "}{el.cantidad}
-            </button>)})}
-			<ul className={store.mostrarCarShop ? "visible"+" list-group" : "oculto" +" list-group"}>
-				{store.carShopping.map((el, index) => {
-					return (
-						// <div className="listado" key={index}>
-							<li key={index} className="list-group-item">
-								{el.nombre+" $"+el.precio+" Cantidad: "+el.cantidad} 
-								<li onClick={() => actions.eliminarCarShop(index)} className="far fa-trash-alt ms-1 p-1" />
-							</li> 
-						// </div>
-					);
-				})}
-				<li className="list-group-item">
-					<Link className="nav-link btn btn-outline-info" onClick={() => actions.setMostrarCarShop()} to="/shopping-cart">Ir al carro</Link>
-				</li>
-			</ul>
-			</div>
-		
-					
-	)
+  useEffect(() => {
+	const sumar = () => {
+	  const sumar = store.carShopping.map((carro, index) => (parseFloat(carro.precio)*parseFloat(carro.cantidad)))
+		.reduce((previous, current) => {
+		  return previous + current;
+		}, 0);
+	  setVSumTotal(sumar);
+	};
+	sumar();
+  });
+
+
+  return (
+    <div className="carShopping">
+      <button
+        id="carShopping"
+        className="btn btn-outline-secondary mt-2 ms-2"
+        onClick={() => actions.setMostrarCarShop()}
+      >
+        
+              <i
+                className="fa-solid fa-cart-shopping fa-2x text-body"
+                key={index}
+              /> <p> {carro.cantidad}</p>
+         
+         
+      </button>
+      <ul
+        className={
+          store.mostrarCarShop
+            ? "visible" + " list-group"
+            : "oculto" + " list-group"
+        }
+      >
+        {store.carShopping.map((el, index) => {
+          return (
+            // <div className="listado" key={index}>
+            <li key={index} className="list-group-item">
+              {el.nombre + " $" + el.precio + " Cantidad: " + el.cantidad}
+              <li
+                onClick={() => actions.eliminarCarShop(index)}
+                className="far fa-trash-alt ms-1 p-1"
+              />
+            </li>
+            // </div>
+          );
+        })}
+        <li className="list-group-item">
+          <Link
+            className="nav-link btn btn-outline-info"
+            onClick={() => actions.setMostrarCarShop()}
+            to="/shopping-cart"
+          >
+            Ir al carro
+          </Link>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default CarShopping;
